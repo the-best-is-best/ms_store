d5c36e351ba7852f38c0e3e40d45e0b783fa2b5d
the-best-is-best <michelle.raouf@outlook.com>
2022-06-10 20:38:22 +0200

    change some files to generation

---
+++ lib/presentation/on_boarding/on_boarding_view_model/on_boarding_view_model_getx.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:ms_store/gen/assets.gen.dart';
+
+import '../../../app/app_refs.dart';
+import '../../../app/di.dart';
+import '../../../domain/models/on_boarding_model.dart';
+import '../../resources/strings_manager.dart';
+
+class OnBoardingController extends GetxController with DataScreen {
+  @override
+  void onInit() {
+    super.onInit();
+    super.pageViewData.value = super._getSliderData();
+  }
+
+  @override
+  void onReady() {
+    Future<bool> showedOnBoarding = AppPrefs.getOnBoarding(onBoarding);
+    showedOnBoarding.then((bool value) {
+      if (value) {
+        //  initHomeModel();
+        //  return Get.offNamed(Routes.homeRoute);
+      }
+    });
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    AppPrefs.updateOnBoarding(onBoarding);
+    super.onClose();
+  }
+}
+
+mixin DataScreen {
+  RxList<OnBoarding?> pageViewData = <OnBoarding?>[].obs;
+  List<OnBoarding> _getSliderData() {
+    return [
+      OnBoarding(
+          title: AppStrings.onBoardingTitle1,
+          description: AppStrings.onBoardingDescription1,
+          urlImage: const $AssetsImagesGen().onboard1.path),
+      OnBoarding(
+          title: AppStrings.onBoardingTitle2,
+          description: AppStrings.onBoardingDescription2,
+          urlImage: const $AssetsImagesGen().onboard2.path),
+      OnBoarding(
+          title: AppStrings.onBoardingTitle3,
+          description: AppStrings.onBoardingDescription3,
+          urlImage: const $AssetsImagesGen().onboard3.path),
+    ];
+  }
+}
